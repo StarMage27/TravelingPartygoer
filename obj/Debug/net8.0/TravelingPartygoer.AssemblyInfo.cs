28eaadcfd29844dd9433e10571ab884b19ef81a8
StarMage27 <36004628+StarMage27@users.noreply.github.com>
2024-09-07 22:13:19 +0700

    1.2

--- obj/Debug/net8.0/TravelingPartygoer.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelingPartygoer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelingPartygoer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432c9e4a25a8aaecdb5abda226d4fb7e57862479")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelingPartygoer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelingPartygoer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
